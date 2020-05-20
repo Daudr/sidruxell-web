5a91e54f8e1a110c967695c96ac1df4b5e840c96
Michele <michele@iMac-di-Michele.station>
2020-05-20 20:51:54 +0200

    fix(article-preview): fix article preview graphic

--- src/components/article-preview/article-preview.js
+++ src/components/article-preview/article-preview.js
@@ -11,71 +11,71 @@ export default ({
   actionText = 'Scopri ora',
   customAction = null,
 }) => (
-  <div className="article__wrapper">
-    {position === 'dx' ? (
-      <div className={`article ${position}`}>
-        <div className="article__body">
-          <h5 className="article__body-subtitle">{article.subtitle}</h5>
-          <h3 className="article__body-title">
-            <strong>{article.title}</strong>
-          </h3>
+    <div className="article__wrapper">
+      {position === 'dx' ? (
+        <div className={`article ${position}`}>
+          <div className="article__body">
+            {article.subtitle && article.subtitle !== '' && <h5 className="article__body-subtitle">{article.subtitle}</h5>}
+            <h3 className="article__body-title">
+              <strong>{article.title}</strong>
+            </h3>
+            <Img
+              className={`article__logo-inside ${position}`}
+              fixed={article.heroImage.fixed}
+            />
+            <div
+              className="article__body-text"
+              dangerouslySetInnerHTML={{
+                __html: article.description.childMarkdownRemark.html,
+              }}
+            ></div>
+            {hasAction && customAction !== null ? (
+              <a href={customAction} rel="noopener norefferer" className={`article__body-action ${position}`}>
+                {actionText}
+              </a>
+            ) : hasAction ? (
+              <Link className={`article__body-action ${position}`} to={`/blog/${article.slug}`}>
+                {actionText}
+              </Link>
+            ) : null}
+          </div>
           <Img
-            className={`article__logo-inside ${position}`}
+            className={`article__logo ${position}`}
             fixed={article.heroImage.fixed}
           />
-          <div
-            className="article__body-text"
-            dangerouslySetInnerHTML={{
-              __html: article.description.childMarkdownRemark.html,
-            }}
-          ></div>
-          {hasAction && customAction !== null ? (
-            <a href={customAction} rel="noopener norefferer" className="article__body-action">
-              {actionText}
-            </a>
-          ) : hasAction ? (
-            <Link className="article__body-action" to={`/blog/${article.slug}`}>
-              {actionText}
-            </Link>
-          ) : null}
         </div>
-        <Img
-          className={`article__logo ${position}`}
-          fixed={article.heroImage.fixed}
-        />
-      </div>
-    ) : (
-      <div className={`article ${position}`}>
-        <Img
-          className={`article__logo ${position}`}
-          fixed={article.heroImage.fixed}
-        />
-        <div className="article__body">
-          <h5 className="article__body-subtitle">{article.subtitle}</h5>
-          <h3 className="article__body-title">
-            <strong>{article.title}</strong>
-          </h3>
-          <Img
-            className={`article__logo-inside ${position}`}
-            fixed={article.heroImage.fixed}
-          />
-          <div
-            className="article__body-text"
-            dangerouslySetInnerHTML={{
-              __html: article.description.childMarkdownRemark.html,
-            }}
-          ></div>
-          {hasAction && customAction !== null ? (
-            <a href={customAction} rel="noopener norefferer" className="article__body-action">
-              {actionText}
-            </a>
-          ) : hasAction ? (
-            <Link className="article__body-action" to={`/blog/${article.slug}`}>
-              {actionText}
-            </Link>
-          ) : null}
-        </div>
-      </div>
-    )}
-  </div>
-)
+      ) : (
+          <div className={`article ${position}`}>
+            <Img
+              className={`article__logo ${position}`}
+              fixed={article.heroImage.fixed}
+            />
+            <div className="article__body">
+              <h5 className="article__body-subtitle">{article.subtitle}</h5>
+              <h3 className="article__body-title">
+                <strong>{article.title}</strong>
+              </h3>
+              <Img
+                className={`article__logo-inside ${position}`}
+                fixed={article.heroImage.fixed}
+              />
+              <div
+                className="article__body-text"
+                dangerouslySetInnerHTML={{
+                  __html: article.description.childMarkdownRemark.html,
+                }}
+              ></div>
+              {hasAction && customAction !== null ? (
+                <a href={customAction} rel="noopener norefferer" className={`article__body-action ${position}`}>
+                  {actionText}
+                </a>
+              ) : hasAction ? (
+                <Link className={`article__body-action ${position}`} to={`/blog/${article.slug}`}>
+                  {actionText}
+                </Link>
+              ) : null}
+            </div>
+          </div>
+        )}
+    </div>
+  )
